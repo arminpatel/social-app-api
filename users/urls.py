22ce9889db73c2cfaa4f98f222cce3a479aeb0b6
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-12-17 17:11:30 +0530

    add endpoint to unfollow user

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .views import FollowCreate
+from .views import FollowCreate, FollowDelete
 
 urlpatterns = [
     path('authenticate/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('follow/<int:following_id>/', FollowCreate.as_view(), name='follow')
+    path('follow/<int:following_id>/', FollowCreate.as_view(), name='follow'),
+    path('unfollow/<int:following_id>/', FollowDelete.as_view(), name='unfollow')
 ]
--- users/views.py
+++ users/views.py
@@ -4,12 +4,13 @@ from django.db import IntegrityError
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import status
+from django.db.models import Q
 
 from .models import UserFollowing
 
 class FollowCreate(APIView):
     permission_class = [IsAuthenticated,]
-    queryset = UserFollowing.objects.all();
+    queryset = UserFollowing.objects.all()
 
     def post(self, request, *args, **kwargs):
         try:
@@ -24,3 +25,23 @@ class FollowCreate(APIView):
             return Response({'message': 'User successfully followed'})
         except IntegrityError:
             return Response({'message': 'User is already followed'})
+    
+class FollowDelete(APIView):
+    permission_class = [IsAuthenticated,]
+    queryset = UserFollowing.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        try:
+            current_user = self.request.user
+        except ValueError:
+            return Response({'message': 'User not authenticated'}, status=status.HTTP_400_BAD_REQUEST)
+
+        unfollowing_user = User.objects.get(pk=self.kwargs['following_id'])
+
+        user_following = UserFollowing.objects.filter(Q(user=current_user) & Q(following_user=unfollowing_user))
+
+        if not user_following:
+            return Response({'message': 'User is not followed'})
+        else:
+            user_following.delete()
+            return Response({'message': 'User unfollowed'}) 
